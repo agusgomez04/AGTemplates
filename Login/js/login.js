36df5d6939165ec11b40545e1ad08308a29186b3
agusgomez04 <agusgomez0409@outlook.com>
2023-03-15 13:55:57 -0300

    Agrego archivos

---
+++ Login/js/login.js
@@ -0,0 +1,12 @@
+function login() {
+    alert("Se ha iniciado sesión correctamente!");
+}
+
+function rememberPassword() {
+    alert("Olvidaste tu contraseña? Obtené una nueva aquí");
+}
+
+function signUp() {
+    alert("Registrate en nuestro sitio aquí!");
+}
+
